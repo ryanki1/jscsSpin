ffd4e485a75c6cacb8abef97bbb6696c93b168ce
Kieran Ryan <kebten@googlemail.com>
2015-07-12 20:13:52 +0200

    jscs test

--- client/app/app.js
+++ client/app/app.js
@@ -7,7 +7,7 @@ angular.module('jscsSpinApp', [
   'ui.router',
   'ui.bootstrap'
 ])
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+  .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
       .otherwise('/');
 
