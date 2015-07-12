468f1ba7cdb52846582549645c0c3b822bb99aa0
Kieran Ryan <kebten@googlemail.com>
2015-07-12 15:25:29 +0200

    Initial commit with: 1. JSCS task 2. Pre-Commit hook calling JSCS task

---
+++ client/components/navbar/navbar.controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('jscsSpinApp')
+  .controller('NavbarCtrl', function ($scope, $location) {
+    $scope.menu = [{
+      'title': 'Home',
+      'link': '/'
+    }];
+
+    $scope.isCollapsed = true;
+
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  });
